b159bf4bc6c6a1d6aa37e8717ad822885d8beabe
sebastian461 <sebastiano-461@hotmail.com>
2024-03-14 17:45:43 -0500

    Mostrar la información del héroe (individual)

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Heroe } from '../interfaces/heroe.interface';
 import { enviroments } from 'src/environments/environments';
 
@@ -12,5 +12,11 @@ export class HeroesService {
 
   getHeroes(): Observable<Heroe[]> {
     return this.httpClient.get<Heroe[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroeById(id: string): Observable<Heroe | undefined> {
+    return this.httpClient
+      .get<Heroe>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
   }
 }
