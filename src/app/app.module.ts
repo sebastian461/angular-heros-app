bc88958e91a44ec2f9a046074463e5fdd2ce48bc
sebastian461 <sebastiano-461@hotmail.com>
2024-03-14 15:20:14 -0500

    Instalación: angular material y prime flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
