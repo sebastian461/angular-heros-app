bff14befaaca5d6edf2c3f836edbfb083b5c7457
sebastian461 <sebastiano-461@hotmail.com>
2024-03-14 17:17:38 -0500

    Card component
    Un componente para mostrar la data obtenida del servicio

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,19 +4,25 @@ import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { MaterialModule } from '../material/material.module';
 
+import { CardComponent } from './components/card/card.component';
 import { HeroePageComponent } from './pages/heroe-page/heroe-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 
+import { HeroeImagePipe } from './pipes/heroe-image.pipe';
+
 @NgModule({
   declarations: [
+    CardComponent,
     HeroePageComponent,
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
+    //Pipes
+    HeroeImagePipe,
   ],
   imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
--- src/app/heroes/interfaces/heroe.interface.ts
+++ src/app/heroes/interfaces/heroe.interface.ts
@@ -5,6 +5,7 @@ export interface Heroe {
   alter_ego: string;
   first_appearance: string;
   characters: string;
+  alt_img?: string;
 }
 
 export enum Publisher {
