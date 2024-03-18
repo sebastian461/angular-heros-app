ccff493367e08dccea7b42c78fb76fff40f6d5ac
sebastian461 <sebastiano-461@hotmail.com>
2024-03-18 17:47:38 -0500

    Actualización del servicio
    Uso del resto de endpoints para crear el crud de los heroes

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Heroe } from '../interfaces/heroe.interface';
 import { enviroments } from 'src/environments/environments';
 
@@ -23,6 +23,25 @@ export class HeroesService {
   getSuggestions(query: string): Observable<Heroe[]> {
     return this.httpClient.get<Heroe[]>(
       `${this.baseUrl}/heroes?q=${query}&_limit=6`
+    );
+  }
+
+  addHeroe(heroe: Heroe): Observable<Heroe> {
+    return this.httpClient.post<Heroe>(`${this.baseUrl}/heroes`, heroe);
+  }
+
+  updateHeroe(heroe: Heroe): Observable<Heroe> {
+    if (!heroe.id) throw Error('Heroe is required');
+    return this.httpClient.patch<Heroe>(
+      `${this.baseUrl}/heroes/${heroe.id}`,
+      heroe
+    );
+  }
+
+  deleteHeroeById(id: string): Observable<boolean> {
+    return this.httpClient.delete(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError((err) => of(false)),
+      map((resp) => true)
     );
   }
 }
